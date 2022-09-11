41f2b9a9f485a8a6f6b1cec6e0c6dfc6dc54cf4f
purushottam123pro <purushottamsingh425@gmail.com>
2022-09-11 08:39:38 +0530

    child process updated

---
+++ 3_nodejs/1_chidprcs.js
@@ -0,0 +1,12 @@
+// child process is a node module used to create sub process within a script
+// you can do different task with your script
+ 
+const cp = require("child_process");
+console.log('opening calculator');
+cp.execSync('calc');
+console.log('opening calculator');
+cp.execSync('code');
+console.log('opening calculator');
+cp.execSync('start chrome');
+cp.execSync('start chrome https://pepcoding.com/');
+
