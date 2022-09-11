f610867699546ce6d4a4837fcb9e1d59b0a648d6
purushottam123pro <purushottamsingh425@gmail.com>
2022-09-11 11:04:41 +0530

    updated

--- 3_nodejs/3_fsmodule.js
+++ 3_nodejs/3_fsmodule.js
@@ -19,13 +19,41 @@ const file = require('fs')
 
 
 
-file.writeFileSync('f2.txt','this is to be added ')
-console.log("data written")
+// file.writeFileSync('f2.txt','this is to be added ')
+// console.log("data written")
 
-file.writeFileSync("f1.txt",'this is override in f1')
-console.log('data overridden')
+// file.writeFileSync("f1.txt",'this is override in f1')
+// console.log('data overridden')
 
-file.writeFileSync("f3.txt", 'it will create new file ')
-console.log('new file created')
+// file.writeFileSync("f3.txt", 'it will create new file ')
+// console.log('new file created')
+
+// append a file 
+// appendfilesync method adds new data toa previously written file
+
+// file.appendFileSync('f2.txt',"add this in f2")
+// console.log('tsxt added in f2')
+
+
+// file.unlinkSync('f2.txt')
+// console.log('f2 deleted')
+ 
+
+// directories 
+
+
+//   creating a new directory
+
+
+// mkdirSync method is used to create a directory of folder
+
+// file.mkdirSync('mydirectory')
+// console.log('directory created ')
+
+file.rmdirSync('mydirectory')
+console.log('directory deleted')
+
+
+
 
 
