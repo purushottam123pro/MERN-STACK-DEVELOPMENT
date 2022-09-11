c7444b2357e58d6f6fe8f0adc3feb3098b8612e4
purushottam123pro <purushottamsingh425@gmail.com>
2022-09-11 09:12:43 +0530

    os updated

--- 3_nodejs/1_chidprcs.js
+++ 3_nodejs/1_chidprcs.js
@@ -2,11 +2,13 @@
 // you can do different task with your script
  
 const cp = require("child_process");
-console.log('opening calculator');
-cp.execSync('calc');
-console.log('opening calculator');
-cp.execSync('code');
-console.log('opening calculator');
-cp.execSync('start chrome');
-cp.execSync('start chrome https://pepcoding.com/');
+// console.log('opening calculator');
+// cp.execSync('calc');
+// console.log('opening calculator');
+// cp.execSync('code');
+// console.log('opening calculator');
+// cp.execSync('start chrome');
+// cp.execSync('start chrome https://pepcoding.com/');
+let output = cp.execSync("node test.js");
+console.log('output ->' + output);
 
